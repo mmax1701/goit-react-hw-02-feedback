bf47655bddf087b05dd04d5749e69a6da52ae812
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-08-06 14:44:20 +0300

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
-import css from './App.module.css';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+
 
 
 
@@ -24,26 +26,29 @@ export class App extends Component {
     return totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
   };
 
+  handleLeaveFeedback = e => {
+    this.setState(prevState => ({
+      [e]: prevState[e] + 1,
+    }));
+  };
+
   render() {
 
     const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
+    const { state, handleLeaveFeedback } = this;
 
     return (
       <div>
-        <section>
-          <h2>Please leave feedback</h2>
-          <div>
-            <button type='button'>Good</button>
-            <button type='button'>Neutral</button>
-            <button type='button'>Bad</button>
-          </div>
-        </section>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={state} onLeaveFeedback={handleLeaveFeedback}></FeedbackOptions>
+        </Section>
+
         <Section title="Statistics">
-          <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />
+          {(total === 0) ? <Notification message="There is no feedback"></Notification> : <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />}
         </Section>
       </div>
-    )
+    );
   }
 }
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types';
 import css from './Section.module.css';
 
-export const Section = ({title}) => {
+export const Section = ({title, children}) => {
     return (
-        <h2>{title}</h2>
+        <section className={css.section}>
+            <h2 className={css.title}>{title}</h2>
+            {children}
+        </section>
     );
 }
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.object.isRequired,
+};
